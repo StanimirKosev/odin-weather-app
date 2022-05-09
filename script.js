11f1f39bae96c281cac129123a8bf609b5571a5b
Stanimir <stanimirkosev00@gmail.com>
2022-05-09 15:59:02 +0300

    user input

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-async function getLocation(locationName) {
-    const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${locationName}&APPID=e34580dee311217873aab3bb6db83b5c`,
-        { mode: 'cors' }
-    )
-    const data = await response.json()
-    // console.log(data)
-    getData(data)
-}
-getLocation('Pleven').catch((error) => {
-    console.error(error)
-})
-
 function getData(data) {
     const myData = {
         city: data.name,
@@ -23,3 +10,22 @@ function getData(data) {
     }
     return myData
 }
+
+async function getLocation(locationName) {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${locationName}&APPID=e34580dee311217873aab3bb6db83b5c`,
+        { mode: 'cors' }
+    )
+    const data = await response.json()
+    getData(data)
+}
+
+function fetchWeather() {
+    const userInput = document.querySelector('input').value
+    getLocation(userInput)
+}
+
+const submitBtn = document.querySelector('button')
+submitBtn.addEventListener('click', () => {
+    fetchWeather()
+})
