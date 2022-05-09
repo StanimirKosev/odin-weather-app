796aa9b16e3cb9d23d64d7f0ca49741bded1d4fe
Stanimir <stanimirkosev00@gmail.com>
2022-05-09 16:25:35 +0300

    info displayed

--- script.js
+++ script.js
@@ -11,13 +11,29 @@ function getData(data) {
     return myData
 }
 
+function displayWeather(newData) {
+    document.querySelector('.city').textContent = newData.city
+    document.querySelector('.country').textContent = newData.country
+    document.querySelector('.weather').textContent = newData.weather
+    document.querySelector('.temperature').textContent = newData.temperature
+    document.querySelector('.feelsLike').textContent = newData.feelsLike
+    document.querySelector('.wind').textContent = newData.wind
+    document.querySelector('.humidity').textContent = newData.humidity
+}
+
 async function getLocation(locationName) {
-    const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${locationName}&APPID=e34580dee311217873aab3bb6db83b5c`,
-        { mode: 'cors' }
-    )
-    const data = await response.json()
-    getData(data)
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${locationName}&APPID=e34580dee311217873aab3bb6db83b5c`,
+            { mode: 'cors' }
+        )
+        const data = await response.json()
+        const newData = getData(data)
+        displayWeather(newData)
+    } catch (error) {
+        document.querySelector('.errorMsg').textContent =
+            'No matching location found!'
+    }
 }
 
 function fetchWeather() {
