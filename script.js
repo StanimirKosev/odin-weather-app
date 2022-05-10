4788f4af678493743e303287de26a4f551e6db5a
Stanimir <stanimirkosev00@gmail.com>
2022-05-10 10:58:00 +0300

    css done

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
+const errorMsg = document.querySelector('.errorMsg')
+const today = new Date()
+
 function getData(data) {
     const myData = {
         city: data.name,
         country: data.sys.country,
-        weather: data.weather[0].main,
-        temperature: data.main.temp,
-        feelsLike: data.main.feels_like,
-        wind: data.wind.speed,
+        weather: data.weather[0].description,
+        temperature: Math.round(data.main.temp - 273.15), // Kelvin to Celsius
+        feelsLike: Math.round(data.main.feels_like - 273.15),
+        wind: Math.round(data.wind.speed * 3.0096), // Beaufort to Km/h
         humidity: data.main.humidity,
     }
     return myData
 }
 
 function displayWeather(newData) {
-    document.querySelector('.city').textContent = newData.city
+    document.querySelector('.time').textContent = today
     document.querySelector('.country').textContent = newData.country
-    document.querySelector('.weather').textContent = newData.weather
     document.querySelector('.temperature').textContent = newData.temperature
-    document.querySelector('.feelsLike').textContent = newData.feelsLike
-    document.querySelector('.wind').textContent = newData.wind
-    document.querySelector('.humidity').textContent = newData.humidity
+    document.querySelector('.wind').textContent = `WIND: ${newData.wind} km/h`
+    document.querySelector('.weather').textContent =
+        newData.weather.toUpperCase()
+    document.querySelector(
+        '.city'
+    ).textContent = `${newData.city.toUpperCase()},`
+    document.querySelector(
+        '.feelsLike'
+    ).textContent = `FEELS LIKE: ${newData.feelsLike}`
+    document.querySelector(
+        '.humidity'
+    ).textContent = `Humidity: ${newData.humidity}%`
 }
 
+// async/await practice
 async function getLocation(locationName) {
     try {
         const response = await fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${locationName}&APPID=e34580dee311217873aab3bb6db83b5c`,
             { mode: 'cors' }
         )
+        errorMsg.classList.remove('active')
         const data = await response.json()
         const newData = getData(data)
         displayWeather(newData)
     } catch (error) {
-        document.querySelector('.errorMsg').textContent =
-            'No matching location found!'
+        errorMsg.classList.add('active')
     }
 }
 
@@ -45,3 +57,20 @@ const submitBtn = document.querySelector('button')
 submitBtn.addEventListener('click', () => {
     fetchWeather()
 })
+
+async function defaultLocation() {
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=Pleven&APPID=e34580dee311217873aab3bb6db83b5c`,
+            { mode: 'cors' }
+        )
+        errorMsg.classList.remove('active')
+        const data = await response.json()
+        const newData = getData(data)
+        displayWeather(newData)
+        console.log(data)
+    } catch (error) {
+        errorMsg.classList.add('active')
+    }
+}
+defaultLocation()
